14ccc052720d80cdbd41c0ae4afd12d8bd2fe42e
Tim Smith <tim.smith.hdg@gmail.com>
2017-04-02 17:52:56 -0700

    setup

---
+++ src/js/client.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Todos from "./pages/Todos";
+import Layout from "./pages/Layout";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Todos}></IndexRoute>
+    </Route>
+  </Router>,
+app);
---
+++ src/js/stores/BankStore.js
@@ -0,0 +1,57 @@
+import { EventEmitter } from "events";
+
+import dispatcher from "../dispatcher";
+
+class BankStore extends EventEmitter {
+  constructor() {
+    super()
+    this.register = [
+      {
+        id: 113464613,
+        text: "Go Shopping",
+        complete: false
+      },
+      {
+        id: 235684679,
+        text: "Pay Water Bill",
+        complete: false
+      },
+    ];
+  }
+
+  createTodo(text) {
+    const id = Date.now();
+
+    this.todos.push({
+      id,
+      text,
+      complete: false,
+    });
+
+    this.emit("change");
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  handleActions(action) {
+    switch(action.type) {
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+        break;
+      }
+      case "RECEIVE_TODOS": {
+        this.todos = action.todos;
+        this.emit("change");
+        break;
+      }
+    }
+  }
+
+}
+
+const bankStore = new BankStore;
+dispatcher.register(bankStore.handleActions.bind(bankStore));
+
+export default bankStore;
